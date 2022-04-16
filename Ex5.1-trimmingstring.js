fff56a0fbb7a90c95852ff29ef61398843d22f58
ShiraOhana <soshiraohana@gmail.com>
2022-04-17 01:06:44 +0300

    solution 5.1

---
+++ Ex5.1-trimmingstring.js
@@ -0,0 +1,10 @@
+// Ex5.1 - trimming string
+// It's pretty straightforward. Your goal is to create a function that removes the first and last
+// characters of a string. You're given one parameter, the original string. You don't have to worry
+// with strings with less than two characters.
+
+function trimming(str) {
+  let trimmedWord = str.slice(1, -1);
+  return trimmedWord;
+}
+console.log(trimming("Hello"));
