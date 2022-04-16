6a7f96ea62cb786d2e6026c87474a3f6e0de2597
ShiraOhana <soshiraohana@gmail.com>
2022-04-16 18:06:56 +0300

    solution 2.4

---
+++ Ex2.4-Unique.js
@@ -0,0 +1,12 @@
+// Ex2.4 - Unique
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+let array = [0, 0, 0.55, 0, 0];
+function findUniq(arr) {
+  arr.sort((a, b) => a - b);
+  return arr[0] === arr[1] ? arr.pop() : arr[0];
+}
+console.log(findUniq(array));
